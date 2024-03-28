8077afbfc124ac05d16def981be0b9c9dd1b4bbf
alejo saez <alejosaezgebicki@gmail.com>
2024-03-28 13:37:32 -0300

    conexion a la base de datos

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,6 +1,14 @@
 import server from "./server";
 import { PORT } from "./config/envs";
+import "reflect-metadata" 
+import{AppDataSource} from "./config/data-source"
 
+
+AppDataSource.initialize()
+.then(res=>{
+console.log("conexion a la base de datos realizada con exito")
 server.listen(PORT, ()=>{
-    console.log(`server listening on port ${PORT}`)
+        console.log(`server listening on port ${PORT}`)
+})
+
 })
